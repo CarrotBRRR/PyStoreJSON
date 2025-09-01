174bb0bf3c7c0099e4b2b69bf234eab32275877b
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-09-01 04:16:42 -0600

    Added Packaging, so pip can use it

---
+++ PyStoreJSONLib/__init__.py
@@ -0,0 +1,4 @@
+from .PyStoreManager import PyStoreManager
+from .PyStoreJSON import PyStoreJSONDB
+
+__all__ = ["PyStoreManager", "PyStoreJSONDB"]
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PyStoreJSON",
+    version="1.0.0",
+    description="A lightweight JSON database and manager for Python",
+    author="CarrotBRRR",
+    packages=["PyStoreJSONLib"],
+    install_requires=[],
+    python_requires=">=3.8",
+)
